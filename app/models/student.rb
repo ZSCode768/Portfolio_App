253bddd9c74bb265e4f430ee0f69af4814c4ad9f
Zack <zsurlow@msudenver.edu>
2024-09-23 16:29:49 -0600

    Currently working on U_3

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,19 @@ class Student < ApplicationRecord
     validates :major, presence: true
     validates :minor, presence: true
     validates :graduation_date, presence: true
+    has_one_attached:profile_picture
+    validate :acceptable_image
+
+    def acceptable_image
+        return unless profile_picture.attached?
+    
+        unless profile_picture.blob.byte_size <= 1.megabyte
+            errors.add(:profile_picture, "is too big")
+        end
+    
+        acceptable_types = ["image/jpeg", "image/png"]
+        unless acceptable_types.include?(profile_picture.content_type)
+            errors.add(:profile_picture, "must be a JPEG or PNG")
+        end
+    end
 end
