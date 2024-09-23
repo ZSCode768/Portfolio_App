6cf3d5f778ccf410801fe7e60125ed5673a35d60
Zack <zsurlow@msudenver.edu>
2024-09-23 15:23:48 -0600

    Fixed it

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_17_192735) do
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "school_email"
+    t.string "major"
+    t.string "minor"
+    t.date "graduation_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should raise error saving student without first name" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(last_name:"Bork",school_email:"b@msudenver.edu",major:"BS")
+    end
+  end
 end
