41b3a8c42aea46cccf1ed108934cdf161eb9e62e
Zack <zsurlow@msudenver.edu>
2024-09-23 16:14:44 -0600

    User Stories 1 & 2 complete

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :school_email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@msudenver\.edu\z/i, message: "Must be an @msudenver.edu email!"}
+    validates :major, presence: true
+    validates :minor, presence: true
+    validates :graduation_date, presence: true
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -3,7 +3,37 @@ require "test_helper"
 class StudentTest < ActiveSupport::TestCase
   test "Should raise error saving student without first name" do
     assert_raises ActiveRecord::RecordInvalid do
-      Student.create!(last_name:"Bork",school_email:"b@msudenver.edu",major:"BS")
+      Student.create!(last_name:"Bork",school_email:"b@msudenver.edu",major:"BS",minor:"ET",graduation_date:"9/23/2024")
+    end
+  end
+  test "Should raise error saving student without last name" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name:"Bark",school_email:"b@msudenver.edu",major:"BS",minor:"ET",graduation_date:"9/23/2024")
+    end
+  end
+  test "Should raise error saving student without email" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name:"Bark",last_name:"Bork",school_email:"invalid-email",major:"BS",minor:"ET",graduation_date:"9/23/2024")
+    end
+  end
+  test "Should raise error saving student with incorrect email" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name:"Bark",last_name:"Bork",major:"BS",minor:"ET",graduation_date:"9/23/2024")
+    end
+  end
+  test "Should raise error saving student without major" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name:"Bark",last_name:"Bork",school_email:"b@msudenver.edu",minor:"ET",graduation_date:"9/23/2024")
+    end
+  end
+  test "Should raise error saving student without minor" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name:"Bark",last_name:"Bork",school_email:"b@msudenver.edu",major:"BS",graduation_date:"9/23/2024")
+    end
+  end
+  test "Should raise error saving student without graduation date" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name:"Bark",last_name:"Bork",school_email:"b@msudenver.edu",major:"BS")
     end
   end
 end
