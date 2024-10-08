ff5e754e5b653c6be14c97719a93b03066e40523
Zack <zsurlow@msudenver.edu>
2024-10-08 12:38:11 -0600

    added 'major' filter

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,6 +9,10 @@ class Student < ApplicationRecord
     # before_create :set_default_image
     validate :acceptable_image
 
+    VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS",
+        "Computer Science BS", "Cybersecurity Major", "Data Science and Machine Learning Major"]
+
+    validates :major, inclusion: {in: VALID_MAJORS, message: "%{value} is not a valid major"}
     # def set_default_image
     #     unless image.attached?
     #         default_image_path = Rails.root.join("app", "assets", "images", "default.png")
