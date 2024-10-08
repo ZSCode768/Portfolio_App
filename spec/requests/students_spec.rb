d32a32c1a364aea6d51f47ad3778e9fcc179d290
Zack <zsurlow@msudenver.edu>
2024-10-08 13:39:38 -0600

    Verified Bootstrap

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -8,3 +8,12 @@ RSpec.describe "Students", type: :request do
     end
   end
 end
+
+RSpec.describe "Students", type: :request do
+  describe "GET /students" do
+    it "returns a successful response" do
+      get students_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
